164dbdc8ab69d1a31ab05bd10e27c2cef28a68f3
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-06-08 14:53:28 -0500

    added scroll header and hrefs

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("myTopnav").style.top = "0";
+  } else {
+    document.getElementById("myTopnav").style.top = "-100px";
+  }
+}
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
